3b054fc06d8cbd1ccb425353ad0897786f73483b
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-05-27 11:40:21 -0700

    developed partial pseudocode skeleton for javascript functionality

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -28,20 +28,31 @@ GAMES
  -player2, string (matches userID)
  -p1Choice, string
  -p2Choice, string
- -status, string
+ -status, string [waiting, playing, complete]
  -winner, string (matches userID)
 */
 
 // on page load, check to see if the user is logged in, if not, show signup/login modal
 
 // if user is signed in,
-// if the user has a current game, show current game
+// hide signup/login modal
+// if the user has a gameNow, show gameNow
 // if the user does not have a current game, show start new game option
 
 /* ---- START NEW GAME ---- */
-// see if there is a game document without a player2 in the games collections
+// see if there is a game document with a "waiting" status in the games collections
 // if so, add player to that game as player2
+// change status to "playing"
+// set player's current game to new game id
+// show game interface
 
-// if there is no "open" game to join
+// if there is no "waiting" game to join
 // create new document in games collecion
-// set player 1 to current player
+// set player1 to current player
+// set player's current game to game id
+// show waiting for another player
+
+/* ---- MAKE CHOICE ---- */
+// allow user to select rock/paper/scissors div
+// set currrent player's choice in game document
+// check to see if both players have chosen, if so, determine winner
